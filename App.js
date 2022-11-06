4ec52c88c03f3edf25de9c716c2ef147583f78c2
kimmingyu <minku4820@gmail.com>
2022-11-07 05:29:28 +0900

    레이아웃 생성 및 플렉스 박스 사용

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Another piece of text!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!"/>
+        <Button title="Add Goal"/>
       </View>
-      <Text
-        style={{ margin: 16, borderWidth: 2, borderColor: "blue", padding: 16 }}
-      >
-        Hello World!
-      </Text>
-      <Button title="Tab me" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
+// 웹에서는 flexDirection 기본값이 row이지만
+// 앱에서는 column 으로 되어있다.
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer:{
+    padding:50,
   },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: "red",
+  inputContainer: {
+    flexDirection:'row',
+    justifyContent:'space-between'
   },
+  textInput: {
+    borderWidth:1,
+    borderColor:'#cccccc',
+    width:'80%',
+    marginRight:8,
+    padding: 8
+  }
 });
