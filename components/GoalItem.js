2666a84a925bc01412c3e7912f9cbce08d5a4309
kimmingyu <minku4820@gmail.com>
2022-11-07 07:30:33 +0900

    목록 클릭 피드백 추가

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 const GoalItme = (props) => {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: "#210644" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -15,12 +19,15 @@ export default GoalItme;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6, //<Text> 태그에 적용시 ios에서 지원이 안돼서 <View>로 감싸줘야 됨.
     // 이러한 사항이 아주 많으므로 공식문서 자주 확인 할 것
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
