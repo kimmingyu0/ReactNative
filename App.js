bf1d5a3f9145f2042d2d6c4708f6f7f5a9c3e3cb
kimmingyu <minku4820@gmail.com>
2022-11-07 05:48:17 +0900

    레이아웃 개선

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!"/>
-        <Button title="Add Goal"/>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -18,18 +18,28 @@ export default function App() {
 // 앱에서는 column 으로 되어있다.
 
 const styles = StyleSheet.create({
-  appContainer:{
-    padding:50,
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
-    flexDirection:'row',
-    justifyContent:'space-between'
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
-    borderWidth:1,
-    borderColor:'#cccccc',
-    width:'80%',
-    marginRight:8,
-    padding: 8
-  }
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
 });
